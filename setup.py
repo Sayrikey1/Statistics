82f4e501bc35e5a12ec70f3bfef0cdb9bf2f3d52
Sayrikey1 <kamalseriki49@gmail.com>
2022-07-06 20:03:03 +0100

    1st commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dists-statistic',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dists-statistics'],
+      author = 'Andrew Paster',
+      author_email='kamalserikki49@gmail.com',
+      zip_safe=False)
